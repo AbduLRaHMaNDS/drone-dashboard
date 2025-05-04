4a95da4813cd6d54b28a4165ec3edac4cd8e1263
Abdulrahman Aldossari <ashr.aldossary@gmail.com>
2025-05-04 23:32:29 +0300

    fix: corrected logo path for Vercel build

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
-import logo from '/home/abdulrahman/Desktop/drone-dashboard/src/assets/logo.png'; // adjust path if needed
+import logo from './assets/logo.png'; // adjust path if needed
 
 const Navbar = () => {
   return (
